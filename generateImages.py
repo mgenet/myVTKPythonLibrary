54b65b57e396a9cb6e3690dfd53310eb9f80f737
Martin Genet <martin.genet@polytechnique.edu>
2016-03-03 16:14:41 +0100

    Adding possibility to warp a mesh

--- generateImages.py
+++ generateImages.py
@@ -28,10 +28,24 @@ class ImageData():
         if (self.structure["type"] == "no"):
             self.I0_structure = self.I0_structure_no
         elif (self.structure["type"] == "heart"):
-            assert (self.images["n_dim"] in (2,3)), "n_dim must be \"2\" or \"3 for \"heart\" type structure. Aborting."
-            self.X_inplane = numpy.array([float()]*2)
-            self.R = float()
-            self.I0_structure = self.I0_structure_heart
+            if (self.images["n_dim"] == 2):
+                self.I0_structure = self.I0_structure_heart_2
+                self.L0 = self.images["L"][0]
+                self.L1 = self.images["L"][1]
+                self.R = float()
+                self.Ri = self.structure["Ri"]
+                self.Re = self.structure["Re"]
+            elif (self.images["n_dim"] == 3):
+                self.I0_structure = self.I0_structure_heart_3
+                self.L0 = self.images["L"][0]
+                self.L1 = self.images["L"][1]
+                self.R = float()
+                self.Ri = self.structure["Ri"]
+                self.Re = self.structure["Re"]
+                self.Zmin = self.structure["Zmin"] if ("Zmin" in self.structure.keys()) else 0.
+                self.Zmax = self.structure["Zmax"] if ("Zmax" in self.structure.keys()) else self.images["L"][2]
+            else:
+                assert (0), "n_dim must be \"2\" or \"3 for \"heart\" type structure. Aborting."
         else:
             assert (0), "structure type must be \"no\" or \"heart\". Aborting."
 
@@ -41,33 +55,48 @@ class ImageData():
         elif (self.texture["type"] == "sine"):
             if   (self.images["n_dim"] == 1):
                 self.I0_texture = self.I0_texture_sine_X
+                self.L0 = self.images["L"][0]
             elif (self.images["n_dim"] == 2):
                 self.I0_texture = self.I0_texture_sine_XY
+                self.L0 = self.images["L"][0]
+                self.L1 = self.images["L"][1]
             elif (self.images["n_dim"] == 3):
                 self.I0_texture = self.I0_texture_sine_XYZ
+                self.L0 = self.images["L"][0]
+                self.L1 = self.images["L"][1]
+                self.L2 = self.images["L"][2]
             else:
                 assert (0), "n_dim must be \"1\", \"2\" or \"3\". Aborting."
         elif (self.texture["type"] == "sinX"):
             self.I0_texture = self.I0_texture_sine_X
+            self.L0 = self.images["L"][0]
         elif (self.texture["type"] == "sinY"):
             self.I0_texture = self.I0_texture_sine_Y
+            self.L1 = self.images["L"][1]
         elif (self.texture["type"] == "sinZ"):
             self.I0_texture = self.I0_texture_sine_Z
+            self.L2 = self.images["L"][2]
         elif (self.texture["type"] == "tagging"):
             if   (self.images["n_dim"] == 1):
                 self.I0_texture = self.I0_texture_tagging_X
+                self.s = self.texture["s"]
             elif (self.images["n_dim"] == 2):
                 self.I0_texture = self.I0_texture_tagging_XY
+                self.s = self.texture["s"]
             elif (self.images["n_dim"] == 3):
                 self.I0_texture = self.I0_texture_tagging_XYZ
+                self.s = self.texture["s"]
             else:
                 assert (0), "n_dim must be \"1\", \"2\" or \"3\". Aborting."
         elif (self.texture["type"] == "taggX"):
             self.I0_texture = self.I0_texture_tagging_X
+            self.s = self.texture["s"]
         elif (self.texture["type"] == "taggY"):
             self.I0_texture = self.I0_texture_tagging_Y
+            self.s = self.texture["s"]
         elif (self.texture["type"] == "taggZ"):
             self.I0_texture = self.I0_texture_tagging_Z
+            self.s = self.texture["s"]
         else:
             assert (0), "texture type must be \"no\", \"sine\", \"sinX\", \"sinY\", \"sinZ\", \"tagging\", \"taggX\", \"taggY\" or \"taggZ\". Aborting."
 
@@ -76,6 +105,8 @@ class ImageData():
             self.I0_noise = self.I0_noise_no
         elif (self.noise["type"] == "normal"):
             self.I0_noise = self.I0_noise_normal
+            self.avg = self.noise["avg"]  if ("avg" in self.noise.keys()) else 0.
+            self.std = self.noise["std"]
         else:
             assert (0), "noise type must be \"no\" or \"normal\". Aborting."
 
@@ -85,11 +116,16 @@ class ImageData():
     def I0_structure_no(self, X):
         return 1.
 
-    def I0_structure_heart(self, X):
-        self.X_inplane[0] = X[0]-self.images["L"][0]/2
-        self.X_inplane[1] = X[1]-self.images["L"][1]/2
-        self.R = numpy.linalg.norm(self.X_inplane)
-        if (self.R >= self.structure["Ri"]) and (self.R <= self.structure["Re"]):
+    def I0_structure_heart_2(self, X):
+        self.R = ((X[0]-self.L0/2)**2 + (X[1]-self.L1/2)**2)**(1./2)
+        if (self.R >= self.Ri) and (self.R <= self.Re):
+            return 1.
+        else:
+            return 0.
+
+    def I0_structure_heart_3(self, X):
+        self.R = ((X[0]-self.L0/2)**2 + (X[1]-self.L1/2)**2)**(1./2)
+        if (self.R >= self.Ri) and (self.R <= self.Re) and (X[2] >= self.Zmin) and (X[2] <= self.Zmax):
             return 1.
         else:
             return 0.
@@ -98,40 +134,40 @@ class ImageData():
         return 1.
 
     def I0_texture_sine_X(self, X):
-        return math.sin(math.pi*X[0]/self.images["L"][0])**2
+        return math.sin(math.pi*X[0]/self.L0)**2
 
     def I0_texture_sine_Y(self, X):
-        return math.sin(math.pi*X[1]/self.images["L"][1])**2
+        return math.sin(math.pi*X[1]/self.L1)**2
 
     def I0_texture_sine_Z(self, X):
-        return math.sin(math.pi*X[2]/self.images["L"][2])**2
+        return math.sin(math.pi*X[2]/self.L2)**2
 
     def I0_texture_sine_XY(self, X):
-        return self.I0_texture_sine_X(X) * self.I0_texture_sine_X(Y)
+        return math.sin(math.pi*X[0]/self.L0)**2 * math.sin(math.pi*X[1]/self.L1)**2
 
     def I0_texture_sine_XYZ(self, X):
-        return self.I0_texture_sine_X(X) * self.I0_texture_sine_X(Y) * self.I0_texture_sine_Z(Y)
+        return math.sin(math.pi*X[0]/self.L0)**2 * math.sin(math.pi*X[1]/self.L1)**2 * math.sin(math.pi*X[2]/self.L2)**2
 
     def I0_texture_tagging_X(self, X):
-        return math.sin(math.pi*X[0]/self.texture["s"])**2
+        return math.sin(math.pi*X[0]/self.s)**2
 
     def I0_texture_tagging_Y(self, X):
-        return math.sin(math.pi*X[1]/self.texture["s"])**2
+        return math.sin(math.pi*X[1]/self.s)**2
 
     def I0_texture_tagging_Z(self, X):
-        return math.sin(math.pi*X[2]/self.texture["s"])**2
+        return math.sin(math.pi*X[2]/self.s)**2
 
     def I0_texture_tagging_XY(self, X):
-        return self.I0_texture_tagging_X(X) * self.I0_texture_tagging_Y(X)
+        return math.sin(math.pi*X[0]/self.s)**2 * math.sin(math.pi*X[1]/self.s)**2
 
     def I0_texture_tagging_XYZ(self, X):
-        return self.I0_texture_tagging_X(X) * self.I0_texture_tagging_Y(X) * self.I0_texture_tagging_Z(X)
+        return math.sin(math.pi*X[0]/self.s)**2 * math.sin(math.pi*X[1]/self.s)**2 * math.sin(math.pi*X[2]/self.s)**2
 
     def I0_noise_no(self):
-        return 0
+        return 0.
 
     def I0_noise_normal(self):
-        return random.normalvariate(self.noise["avg"], self.noise["std"])
+        return random.normalvariate(self.avg, self.std)
 
 ########################################################################
 
@@ -145,17 +181,24 @@ class Mapping:
         if (self.deformation["type"] == "no"):
             self.init_t = self.init_t_no
             self.X = self.X_no
+            self.x = self.x_no
         elif (self.deformation["type"] == "homogeneous"):
             self.init_t = self.init_t_homogeneous
             self.X = self.X_homogeneous
+            self.x = self.x_homogeneous
         elif (self.deformation["type"] == "heart"):
             assert (self.structure["type"] == "heart"), "structure type must be \"heart\" for \"heart\" type deformation. Aborting."
             self.init_t = self.init_t_heart
-            self.x_inplane = numpy.array([float()]*2)
-            self.rt = numpy.array([float()]*2)
-            self.RT = numpy.array([float()]*2)
-            self.X_full = numpy.array([float()]*3)
             self.X = self.X_heart
+            self.x = self.x_heart
+            self.x_inplane = numpy.empty(2)
+            self.X_inplane = numpy.empty(2)
+            self.rt = numpy.empty(2)
+            self.RT = numpy.empty(2)
+            self.X_full = numpy.empty(3)
+            self.x_full = numpy.empty(3)
+            self.L0 = self.images["L"][0]
+            self.L1 = self.images["L"][1]
         else:
             assert (0), "deformation type must be \"no\", \"homogeneous\" or \"heart\". Aborting."
 
@@ -211,19 +254,41 @@ class Mapping:
 
     def X_heart(self, x):
         #print "x = " + str(x)
-        self.x_inplane[0] = x[0] - self.images["L"][0]/2
-        self.x_inplane[1] = x[1] - self.images["L"][1]/2
+        self.x_inplane[0] = x[0] - self.L0/2
+        self.x_inplane[1] = x[1] - self.L1/2
         #print "x_inplane = " + str(self.x_inplane)
         self.rt[0] = numpy.linalg.norm(self.x_inplane)
         self.rt[1] = math.atan2(self.x_inplane[1], self.x_inplane[0])
         #print "rt = " + str(self.rt)
         self.RT[:] = numpy.dot(self.Ainv, self.rt-self.B)
         #print "RT = " + str(self.RT)
-        self.X_full[0] = self.RT[0] * math.cos(self.RT[1]) + self.images["L"][0]/2
-        self.X_full[1] = self.RT[0] * math.sin(self.RT[1]) + self.images["L"][1]/2
+        self.X_full[0] = self.RT[0] * math.cos(self.RT[1]) + self.L0/2
+        self.X_full[1] = self.RT[0] * math.sin(self.RT[1]) + self.L1/2
         self.X_full[2] = x[2]
         #print "X_full = " + str(self.X_full)
         return self.X_full
+
+    def x_no(self, X):
+        return X
+
+    def x_homogeneous(self, X):
+        return numpy.dot(self.F, X)
+
+    def x_heart(self, X):
+        #print "X = " + str(X)
+        self.X_inplane[0] = X[0] - self.L0/2
+        self.X_inplane[1] = X[1] - self.L1/2
+        #print "X_inplane = " + str(self.X_inplane)
+        self.RT[0] = numpy.linalg.norm(self.X_inplane)
+        self.RT[1] = math.atan2(self.X_inplane[1], self.X_inplane[0])
+        #print "RT = " + str(self.RT)
+        self.rt[:] = numpy.dot(self.A, self.RT) + self.B
+        #print "rt = " + str(self.rt)
+        self.x_full[0] = self.rt[0] * math.cos(self.rt[1]) + self.L0/2
+        self.x_full[1] = self.rt[0] * math.sin(self.rt[1]) + self.L1/2
+        self.x_full[2] = X[2]
+        #print "x_full = " + str(self.x_full)
+        return self.x_full
 
 ########################################################################
 
@@ -263,19 +328,14 @@ def generateImages(
     elif (images["n_dim"] == 2):
         origin[2] = 0.
     image.SetOrigin(origin)
-
-    array_data = myVTK.createFloatArray(
-        name="scalars",
-        n_components=1,
-        n_tuples=image.GetNumberOfPoints())
-    image.GetPointData().AddArray(array_data)
-    image.GetPointData().SetActiveScalars("scalars")
+    image.AllocateScalars(vtk.VTK_FLOAT, 1)
+    image_scalars = image.GetPointData().GetScalars()
 
     if not os.path.exists(images["images_folder"]):
         os.mkdir(images["images_folder"])
 
-    x0 = numpy.array([float()]*3)
-    x  = numpy.array([float()]*3)
+    x0 = numpy.empty(3)
+    x  = numpy.empty(3)
     if   (images["n_dim"] == 1):
         dx = spacing[0]/images["n_int"][0]
     elif (images["n_dim"] == 2):
@@ -291,8 +351,8 @@ def generateImages(
     global_max = float("-Inf")
     image_data = ImageData(images, structure, texture, noise)
     mapping = Mapping(images, structure, deformation, evolution)
-    for k_frame in xrange(images["n_frames"]+1):
-        t = images["T"]*float(k_frame)/images["n_frames"]
+    for k_frame in xrange(images["n_frames"]):
+        t = images["T"]*float(k_frame)/(images["n_frames"]-1)
         mapping.init_t(t)
         for k_point in xrange(image.GetNumberOfPoints()):
             image.GetPoint(k_point, x0)
@@ -324,7 +384,7 @@ def generateImages(
                 I /= images["n_int"][2]*images["n_int"][1]*images["n_int"][0]
             else:
                 assert (0), "n_dim must be \"1\", \"2\" or \"3\". Aborting."
-            array_data.SetTuple(k_point, [I])
+            image_scalars.SetTuple1(k_point, I)
             if (I < global_min): global_min = I
             if (I > global_max): global_max = I
         myVTK.writeImage(
@@ -333,7 +393,7 @@ def generateImages(
 
     if (images["data_type"] in ("float")):
         pass
-    elif (images["data_type"] in ("unsigned char", "unsigned short", "unsigned int", "unsigned long", "uint8", "uint16", "uint32", "uint64")):
+    elif (images["data_type"] in ("unsigned char", "unsigned short", "unsigned int", "unsigned long", "unsigned float" "uint8", "uint16", "uint32", "uint64", "ufloat")):
         print "global_min = " + str(global_min)
         print "global_max = " + str(global_max)
         shifter = vtk.vtkImageShiftScale()
@@ -350,8 +410,12 @@ def generateImages(
         elif (images["data_type"] in ("unsigned long", "uint64")):
             shifter.SetScale(float(2**64-1)/(global_max-global_min))
             shifter.SetOutputScalarTypeToUnsignedLong()
-        for k_frame in xrange(images["n_frames"]+1):
-            image = myVTK.readImage(filename=images["images_folder"]+"/"+images["images_basename"]+"_"+str(k_frame).zfill(2)+".vti")
+        elif (images["data_type"] in ("unsigned float", "ufloat")):
+            shifter.SetScale(1./(global_max-global_min))
+            shifter.SetOutputScalarTypeToFloat()
+        for k_frame in xrange(images["n_frames"]):
+            image = myVTK.readImage(
+                filename=images["images_folder"]+"/"+images["images_basename"]+"_"+str(k_frame).zfill(2)+".vti")
             if (vtk.vtkVersion.GetVTKMajorVersion() >= 6):
                 shifter.SetInputData(image)
             else:
@@ -363,3 +427,67 @@ def generateImages(
                 filename=images["images_folder"]+"/"+images["images_basename"]+"_"+str(k_frame).zfill(2)+".vti")
     else:
         assert (0), "Wrong data type. Aborting."
+
+########################################################################
+
+def warpMesh(
+        mesh_basename,
+        images,
+        structure,
+        deformation,
+        evolution,
+        verbose=1):
+
+    myVTK.myPrint(verbose, "*** warpMesh ***")
+
+    mesh = myVTK.readUGrid(
+        filename=images["images_folder"]+"/"+mesh_basename+".vtk",
+        verbose=0)
+    n_points = mesh.GetNumberOfPoints()
+    n_cells = mesh.GetNumberOfCells()
+
+    farray_disp = myVTK.createFloatArray(
+        name="displacement",
+        n_components=3,
+        n_tuples=n_points,
+        verbose=0)
+    mesh.GetPointData().AddArray(farray_disp)
+
+    mapping = Mapping(images, structure, deformation, evolution)
+
+    X = numpy.empty(3)
+    x = numpy.empty(3)
+    U = numpy.empty(3)
+    for k_frame in xrange(images["n_frames"]):
+        t = images["T"]*float(k_frame)/(images["n_frames"]-1)
+        mapping.init_t(t)
+
+        for k_point in xrange(n_points):
+            mesh.GetPoint(k_point, X)
+            x[:] = mapping.x(X)
+            U[:] = x[:] - X[:]
+            farray_disp.SetTuple(k_point, U)
+
+        myVTK.writeUGrid(
+            ugrid=mesh,
+            filename=images["images_folder"]+"/"+mesh_basename+"_"+str(k_frame).zfill(2)+".vtk",
+            verbose=0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
