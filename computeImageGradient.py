6b8fc794798904fad56b04238a294b7c7b7983a4
Martin Genet <martin.genet@polytechnique.edu>
2016-03-22 14:00:22 +0100

    Correct image gradient dimensionality

--- computeImageGradient.py
+++ computeImageGradient.py
@@ -27,6 +27,18 @@ def computeImageGradient(
         image_gradient.SetInputData(image)
     else:
         image_gradient.SetInput(image)
+    extent = image.GetExtent()
+    DX = extent[1]+1-extent[0]
+    DY = extent[3]+1-extent[2]
+    DZ = extent[5]+1-extent[4]
+    if   (DX > 1) and (DY > 1) and (DZ > 1):
+        image_gradient.SetDimensionality(3)
+    elif (DX > 1) and (DY > 1) and (DZ == 1):
+        image_gradient.SetDimensionality(2)
+    elif (DX > 1) and (DY == 1) and (DZ == 1):
+        image_gradient.SetDimensionality(1)
+    else:
+        assert (0), "Wrong image dimensionality ("+str(extent)+")"
     image_gradient.Update()
     image = image_gradient.GetOutput()
 
