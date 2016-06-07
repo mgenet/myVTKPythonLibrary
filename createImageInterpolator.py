0ee0b8fad7979fe6a1c44d186485b438d2314337
Martin Genet <martin.genet@polytechnique.edu>
2016-06-07 13:58:14 +0200

    option to choose the image interpolator mode

--- createImageInterpolator.py
+++ createImageInterpolator.py
@@ -18,15 +18,23 @@ import myVTKPythonLibrary as myVTK
 
 def createImageInterpolator(
         image,
+        mode="linear"
         out_value=None,
-        verbose=1):
+        verbose=0):
 
     myVTK.myPrint(verbose, "*** createImageInterpolator ***")
 
     interpolator = vtk.vtkImageInterpolator()
-    interpolator.Initialize(image)
+    assert (mode in ("nearest", "linear", "cubic"))
+    if (mode == "nearest"):
+        interpolator.SetInterpolationModeToNearest()
+    elif (mode == "linear"):
+        interpolator.SetInterpolationModeToLinear()
+    elif (mode == "cubic"):
+        interpolator.SetInterpolationModeToCubic()
     if (out_value is not None):
         interpolator.SetOutValue(out_value)
+    interpolator.Initialize(image)
     interpolator.Update()
 
     return interpolator
