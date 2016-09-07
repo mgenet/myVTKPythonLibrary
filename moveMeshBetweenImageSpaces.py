57a8f3259c062347fa6519b9900087080a103020
Martin Genet <martin.genet@polytechnique.edu>
2016-09-07 15:26:00 +0200

    Make input/output image spaces optional

--- moveMeshBetweenImageSpaces.py
+++ moveMeshBetweenImageSpaces.py
@@ -22,19 +22,25 @@ import myVTKPythonLibrary as myvtk
 if (__name__ == "__main__"):
     parser = argparse.ArgumentParser()
     parser.add_argument('mesh_in_filename', type=str)
-    parser.add_argument('image_in', type=str)
-    parser.add_argument('image_out', type=str)
+    parser.add_argument('--image_in', type=str, default=None)
+    parser.add_argument('--image_out', type=str, default=None)
     parser.add_argument('--mesh_out_filename', type=str, default=None)
     parser.add_argument('-v', '--verbose', type=int, default=1)
     args = parser.parse_args()
 
-    S_in = numpy.diag(list(numpy.loadtxt("Image_"+args.image_in+"_Scaling.dat"))+[1])
-    W_in = numpy.loadtxt("Image_"+args.image_in+"_WorldMatrix.dat")
-    I2W_in = numpy.dot(W_in, numpy.linalg.inv(S_in))
+    if (image_in is not None):
+        S_in = numpy.diag(list(numpy.loadtxt("Image_"+args.image_in+"_Scaling.dat"))+[1])
+        W_in = numpy.loadtxt("Image_"+args.image_in+"_WorldMatrix.dat")
+        I2W_in = numpy.dot(W_in, numpy.linalg.inv(S_in))
+    else:
+        I2W_in = numpy.eye(4)
 
-    S_out = numpy.diag(list(numpy.loadtxt("Image_"+args.image_out+"_Scaling.dat"))+[1])
-    W_out = numpy.loadtxt("Image_"+args.image_out+"_WorldMatrix.dat")
-    I2W_out = numpy.dot(W_out, numpy.linalg.inv(S_out))
+    if (image_out is not None):
+        S_out = numpy.diag(list(numpy.loadtxt("Image_"+args.image_out+"_Scaling.dat"))+[1])
+        W_out = numpy.loadtxt("Image_"+args.image_out+"_WorldMatrix.dat")
+        I2W_out = numpy.dot(W_out, numpy.linalg.inv(S_out))
+    else:
+        I2W_out = numpy.eye(4)
 
     I2I = numpy.dot(numpy.linalg.inv(I2W_out), I2W_in)
 
