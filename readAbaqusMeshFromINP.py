6e6c312e385eff11fb7824e16a6dcd755ee98a5f
Martin Genet <martin.genet@polytechnique.edu>
2017-01-23 15:10:31 +0100

    2016 -> 2017

--- readAbaqusMeshFromINP.py
+++ readAbaqusMeshFromINP.py
@@ -2,7 +2,7 @@
 
 ########################################################################
 ###                                                                  ###
-### Created by Martin Genet, 2012-2016                               ###
+### Created by Martin Genet, 2012-2017                               ###
 ###                                                                  ###
 ### University of California at San Francisco (UCSF), USA            ###
 ### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland ###
@@ -31,41 +31,43 @@ def readAbaqusMeshFromINP(
 
     context = ""
     for line in mesh_file:
-        if (line[-1:] == "\n"): line = line[:-1]
-        #mypy.my_print(verbose-1, "line =", line)
-
         if line.startswith("**"): continue
 
-        if (context == "reading nodes"):
-            if line.startswith("*"):
-                context = ""
-            else:
-                splitted_line = line.split(",")
-                points.InsertNextPoint([float(coord) for coord in splitted_line[1:4]])
+        line = line.strip("\n ,")
+        #mypy.my_print(verbose-1, "line =", line)
 
-        if (context == "reading elems"):
+        if (context != ""):
             if line.startswith("*"):
                 context = ""
             else:
-                splitted_line = line.split(",")
-                assert (len(splitted_line) == 1+cell_n_points), "Wrong number of elements in line. Aborting."
-                for k_point in xrange(cell_n_points): cell.GetPointIds().SetId(k_point, int(splitted_line[1+k_point])-1)
-                cell_array.InsertNextCell(cell)
+                if (context == "reading nodes"):
+                    splitted_line = line.split(",")
+                    points.InsertNextPoint([float(coord) for coord in splitted_line[1:4]])
+                elif (context == "reading elems"):
+                    splitted_line = line.split(",")
+                    assert (len(splitted_line) == 1+cell_n_points), "Wrong number of elements in line. Aborting."
+                    for k_point in xrange(cell_n_points): cell.GetPointIds().SetId(k_point, int(splitted_line[1+k_point])-1)
+                    cell_array.InsertNextCell(cell)
 
         if line.startswith("*NODE"):
             context = "reading nodes"
-        if line.startswith("*ELEMENT"):
-            if ("TYPE=F3D4" in line) and (("quad" in elem_types) or ("all" in elem_types)):
+        elif line.startswith("*ELEMENT"):
+            if (("TYPE=CPS3" in line) or ("type=CPS3" in line) or ("TYPE=CPE3" in line) or ("type=CPE3" in line)) and (("tri" in elem_types) or (elem_types == "all")):
+                context = "reading elems"
+                cell_vtk_type = vtk.VTK_TRIANGLE
+                cell_n_points = 3
+                cell = vtk.vtkTriangle()
+            elif (("TYPE=F3D4" in line) or ("type=F3D4" in line)) and (("quad" in elem_types) or (elem_types == "all")):
                 context = "reading elems"
                 cell_vtk_type = vtk.VTK_QUAD
                 cell_n_points = 4
                 cell = vtk.vtkQuad()
-            elif ("TYPE=C3D4" in line) and (("tet" in elem_types) or ("all" in elem_types)):
+            elif (("TYPE=C3D4" in line) or ("type=C3D4" in line)) and (("tet" in elem_types) or (elem_types == "all")):
                 context = "reading elems"
                 cell_vtk_type = vtk.VTK_TETRA
                 cell_n_points = 4
                 cell = vtk.vtkTetra()
-            elif ("TYPE=C3D8" in line) and (("hex" in elem_types) or ("all" in elem_types)):
+            elif (("TYPE=C3D8" in line) or ("type=C3D8" in line)) and (("hex" in elem_types) or (elem_types == "all")):
                 context = "reading elems"
                 cell_vtk_type = vtk.VTK_HEXAHEDRON
                 cell_n_points = 8
