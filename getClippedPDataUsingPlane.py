b5bf8b7e34e639652b44666cd8986e6625bb1d77
Martin Genet <martin.genet@polytechnique.edu>
2017-12-08 23:38:03 +0100

    fix weird bug in getClippedPDataUsingPlane

--- getClippedPDataUsingPlane.py
+++ getClippedPDataUsingPlane.py
@@ -21,6 +21,7 @@ def getClippedPDataUsingPlane(
         pdata_mesh,
         plane_O,
         plane_N,
+        generate_clipped_output=False, # 2017/12/01: this seems to mess up everything with the returned pointers…
         verbose=0):
 
     mypy.my_print(verbose, "*** getClippedPDataUsingPlane ***")
@@ -35,21 +36,30 @@ def getClippedPDataUsingPlane(
 
     clip = vtk.vtkClipPolyData()
     clip.SetClipFunction(plane)
-    clip.GenerateClippedOutputOn()
+    if (generate_clipped_output):
+        clip.GenerateClippedOutputOn()
     if (vtk.vtkVersion.GetVTKMajorVersion() >= 6):
         clip.SetInputData(pdata_mesh)
     else:
         clip.SetInput(pdata_mesh)
     clip.Update()
-    clipped0 = clip.GetOutput(0)
-    clipped1 = clip.GetOutput(1)
+    if (generate_clipped_output):
+        clipped0 = clip.GetOutput(0)
+        clipped1 = clip.GetOutput(1)
+    else:
+        clipped = clip.GetOutput()
 
     #mypy.my_print(verbose-1, "clipped0.GetNumberOfPoints() = "+str(clipped0.GetNumberOfPoints()))
     #mypy.my_print(verbose-1, "clipped1.GetNumberOfPoints() = "+str(clipped1.GetNumberOfPoints()))
     #mypy.my_print(verbose-1, "clipped0.GetNumberOfCells() = "+str(clipped0.GetNumberOfCells()))
     #mypy.my_print(verbose-1, "clipped1.GetNumberOfCells() = "+str(clipped1.GetNumberOfCells()))
 
-    if (clipped0.GetNumberOfCells() > clipped1.GetNumberOfCells()):
+    #if (myvtk.getPDataSurfaceArea(clipped0) >= myvtk.getPDataSurfaceArea(clipped1)):
+        #return clipped0, clipped1
+    #else:
+        #return clipped1, clipped0
+
+    if (generate_clipped_output):
         return clipped0, clipped1
     else:
-        return clipped1, clipped0
+        return clipped
