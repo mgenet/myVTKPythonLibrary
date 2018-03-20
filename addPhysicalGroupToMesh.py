2b61796aec9b4d38b81f7d4afd29774e158f0cdd
Martin Genet <martin.genet@polytechnique.edu>
2018-03-20 11:47:59 +0100

    2017 -> 2018

--- addPhysicalGroupToMesh.py
+++ addPhysicalGroupToMesh.py
@@ -3,7 +3,7 @@
 
 ########################################################################
 ###                                                                  ###
-### Created by Martin Genet, 2012-2017                               ###
+### Created by Martin Genet, 2012-2018                               ###
 ###     (Inspired by Lik Chuan Lee)                                  ###
 ###                                                                  ###
 ### University of California at San Francisco (UCSF), USA            ###
