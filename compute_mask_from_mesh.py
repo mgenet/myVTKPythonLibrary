a0506e119cb82f505ddac598e4b48bcf7e158767
Cécile Patte <cecile.patte@inria.fr>
2019-04-18 12:11:35 +0200

    arguments are now objects

--- compute_mask_from_mesh.py
+++ compute_mask_from_mesh.py
@@ -1,14 +1,19 @@
 #coding=utf8
 
-################################################################################
-###                                                                          ###
-### Created by Martin Genet, 2012-2016                                       ###
-###                                                                          ###
-### University of California at San Francisco (UCSF), USA                    ###
-### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland         ###
-### École Polytechnique, Palaiseau, France                                   ###
-###                                                                          ###
-################################################################################
+########################################################################
+###                                                                  ###
+### Created by Martin Genet, 2012-2019                               ###
+###                                                                  ###
+### University of California at San Francisco (UCSF), USA            ###
+### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland ###
+### École Polytechnique, Palaiseau, France                           ###
+###                                                                  ###
+###                                                                  ###
+### And Cécile Patte, 2019                                          ###
+###                                                                  ###
+### INRIA, Palaiseau, France                                         ###
+###                                                                  ###
+########################################################################
 
 import glob
 import numpy
@@ -19,74 +24,48 @@ import myVTKPythonLibrary as myvtk
 
 ################################################################################
 
-def compute_mask_from_mesh(ref_image_folder,
-                           ref_image_basename,
-                           working_folder,
-                           working_basename,
-                           ref_frame=0,
-                           working_ext="vtu",
+def compute_mask_from_mesh(image,
+                           mesh,
                            warp_mesh=1,
-                           working_displacement_field_name="U",
+                           mesh_displacement_field_name="U",
                            binary_mask=1,
                            verbose=0):
 
-    ref_image_zfill = len(glob.glob(ref_image_folder+"/"+ref_image_basename+"_*.vti")[0].rsplit("_")[-1].split(".")[0])
-    ref_image_filename = ref_image_folder+"/"+ref_image_basename+"_"+str(ref_frame).zfill(ref_image_zfill)+".vti"
-    ref_image = myvtk.readImage(
-        filename=ref_image_filename)
-
-    working_zfill = len(glob.glob(working_folder+"/"+working_basename+"_*."+working_ext)[0].rsplit("_")[-1].split(".")[0])
-    n_frames = len(glob.glob(working_folder+"/"+working_basename+"_"+"[0-9]"*working_zfill+"."+working_ext))
-
     if binary_mask:
         thres = vtk.vtkImageThreshold()
-        thres.SetInputData(ref_image)
+        thres.SetInputData(image)
         thres.ThresholdByUpper(0.)
         thres.SetInValue(1.0)
         thres.ReplaceInOn()
         thres.Update()
         image = thres.GetOutput()
+
+    assert (mesh.GetPointData().HasArray(mesh_displacement_field_name)), "no array '" + mesh_displacement_field_name + "' in mesh"
+    mesh.GetPointData().SetActiveVectors(mesh_displacement_field_name)
+
+    geom = vtk.vtkGeometryFilter()
+    if warp_mesh:
+        mesh.GetPointData().SetActiveVectors(mesh_displacement_field_name)
+        warp = vtk.vtkWarpVector()
+        warp.SetInputData(mesh)
+        warp.Update()
+        geom.SetInputData(warp.GetOutput())
     else:
-        n_images = len(glob.glob(ref_image_folder+"/"+ref_image_basename+"_"+"[0-9]"*ref_image_zfill+".vti"))
-        assert n_images == n_frames
+        geom.SetInputData(mesh)
+    geom.Update()
 
-    for k_frame in xrange(n_frames):
-        mypy.my_print(verbose, "k_frame = "+str(k_frame))
+    pol2stenc = vtk.vtkPolyDataToImageStencil()
+    pol2stenc.SetInputData(geom.GetOutput())
+    pol2stenc.SetOutputOrigin(image.GetOrigin())
+    pol2stenc.SetOutputSpacing(image.GetSpacing())
+    pol2stenc.SetOutputWholeExtent(image.GetExtent())
+    pol2stenc.Update()
 
-        mesh = myvtk.readUGrid(
-            filename=working_folder+"/"+working_basename+"_"+str(k_frame).zfill(working_zfill)+"."+working_ext)
-        assert (mesh.GetPointData().HasArray(working_displacement_field_name)), "no array '" + working_displacement_field_name + "' in mesh"
-        mesh.GetPointData().SetActiveVectors(working_displacement_field_name)
+    imgstenc = vtk.vtkImageStencil()
+    imgstenc.SetInputData(image)
+    imgstenc.SetStencilData(pol2stenc.GetOutput())
+    imgstenc.ReverseStencilOff()
+    imgstenc.SetBackgroundValue(0)
+    imgstenc.Update()
 
-        if not binary_mask:
-            image = myvtk.readImage(
-                filename=ref_image_folder+"/"+ref_image_basename+"_"+str(k_frame).zfill(ref_image_zfill)+".vti")
-
-        geom = vtk.vtkGeometryFilter()
-        if warp_mesh:
-            mesh.GetPointData().SetActiveVectors(working_displacement_field_name)
-            warp = vtk.vtkWarpVector()
-            warp.SetInputData(mesh)
-            warp.Update()
-            geom.SetInputConnection(warp.GetOutputPort())
-        else:
-            geom.SetInputData(mesh)
-        geom.Update()
-
-        pol2stenc = vtk.vtkPolyDataToImageStencil()
-        pol2stenc.SetInputConnection(geom.GetOutputPort())
-        pol2stenc.SetOutputOrigin(ref_image.GetOrigin())
-        pol2stenc.SetOutputSpacing(ref_image.GetSpacing())
-        pol2stenc.SetOutputWholeExtent(ref_image.GetExtent())
-        pol2stenc.Update()
-
-        imgstenc = vtk.vtkImageStencil()
-        imgstenc.SetInputData(image)
-        imgstenc.SetStencilConnection(pol2stenc.GetOutputPort())
-        imgstenc.ReverseStencilOff()
-        imgstenc.SetBackgroundValue(0)
-        imgstenc.Update()
-
-        myvtk.writeImage(
-            image=imgstenc.GetOutput(),
-            filename=working_folder+"/"+working_basename+"-mask_"+str(k_frame).zfill(working_zfill)+".vti")
+    return imgstenc.GetOutput()
