b733cc3dc0a56bb20451a3cfd123edf1b5f3f55c
Martin Genet <martin.genet@polytechnique.edu>
2025-04-17 15:02:15 +0200

    2023 -> 2025

--- myVTKPythonLibrary/createIntArray.py
+++ myVTKPythonLibrary/createIntArray.py
@@ -2,7 +2,7 @@
 
 ########################################################################
 ###                                                                  ###
-### Created by Martin Genet, 2012-2023                               ###
+### Created by Martin Genet, 2012-2025                               ###
 ###                                                                  ###
 ### University of California at San Francisco (UCSF), USA            ###
 ### Swiss Federal Institute of Technology (ETH), Zurich, Switzerland ###
