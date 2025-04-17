acee1c35e846cbc6481f35e77da978fa8ec6977b
Martin Genet <martin.genet@polytechnique.edu>
2025-04-17 14:10:53 +0200

    2023 -> 2025

--- myVTKPythonLibrary/addPhysicalGroupToMesh.py
+++ myVTKPythonLibrary/addPhysicalGroupToMesh.py
@@ -3,7 +3,7 @@
 
 ########################################################################
 ###                                                                  ###
-### Created by Martin Genet, 2012-2023                               ###
+### Created by Martin Genet, 2012-2025                               ###
 ###     (Inspired by Lik Chuan Lee)                                  ###
 ###                                                                  ###
 ### University of California at San Francisco (UCSF), USA            ###
